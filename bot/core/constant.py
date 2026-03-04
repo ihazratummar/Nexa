5bbc77b22d751cedffb5620b98030d55d3126535
Hazrat Ummar Shaikh <hazratummar9@gmail.com>
2026-03-04 12:52:33 +0530

    Added Rate Limit and redis

--- bot/core/constant.py
+++ bot/core/constant.py
@@ -36,4 +36,4 @@ class DbCons(Enum):
     COMMAND_SETTINGS = "command_settings"
     MOD_SETTINGS = "moderation_settings"
     AUTOMOD_SETTINGS = "automod_settings"
-    USER_COLLECTION = "users"
+    USER_COLLECTION = "user_collection"
